5d15a0ef267c42522da5740ec1a17ac5150a3da9
Kipras <kipras.st@gmail.com>
2023-04-06 00:54:00 +0300

    Updating tests.

--- DrivingChargesApi.Tests/CongestionCharges/CongestionRepositoryTests.cs
+++ DrivingChargesApi.Tests/CongestionCharges/CongestionRepositoryTests.cs
@@ -1,12 +1,60 @@
-﻿using System;
+﻿using DrivingChargesApi.Tests.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using DrivingChargesApi.Data;
+using DrivingChargesApi.CongestionCharges;
+using DrivingChargesApi.Data.CongestionData;
+using Microsoft.EntityFrameworkCore;
+using FluentAssertions;
 
 namespace DrivingChargesApi.Tests.CongestionCharges
 {
     public class CongestionRepositoryTests
     {
+        private readonly Mock<ChargeContext> _chargeContextMock;
+        private readonly CongestionRepository _congestionRepository;
+
+        public CongestionRepositoryTests()
+        {
+            _chargeContextMock = new Mock<ChargeContext>();
+            _congestionRepository = new CongestionRepository(_chargeContextMock.Object);
+        }
+
+        [Fact]
+        public async void ShouldGetCongestionTypes()
+        {
+            var congestions = new List<Congestion>()
+            {
+                new Congestion
+                {
+                    Id = 1,
+                    Type = "WeekDay",
+                    Coefficient = 1,
+                    CityId = 1,
+                    Periods = new()
+                },
+                new Congestion
+                {
+                    Id = 2,
+                    Type = "WeekEnd",
+                    Coefficient = 1,
+                    CityId = 1,
+                    Periods = new()
+                }
+            };
+
+            _chargeContextMock.Setup(m => m.Congestions.ToList()).Returns(congestions);
+
+            var cityName = "London";
+            var result = await _congestionRepository.CongestionTypes(cityName);
+
+            result.Should().Contain("WeekDay");
+            result.Should().Contain("WeekEnd");
+        }
     }
 }
---
+++ DrivingChargesApi.Tests/Data/TestChargeContext.cs
@@ -0,0 +1,16 @@
+﻿using DrivingChargesApi.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+
+namespace DrivingChargesApi.Tests.Data
+{
+    public class TestChargeContext
+    {
+        public ChargeContext InMemory() =>
+            new ChargeContext(
+                new DbContextOptionsBuilder<ChargeContext>().
+                    UseInMemoryDatabase(Guid.NewGuid().ToString()).
+                    Options);
+    }
+}
